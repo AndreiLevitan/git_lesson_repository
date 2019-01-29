e84ac8e823fee33a8b232a4db382f0d71fc03af9
AndreiLevitan <45385479+AndreiLevitan@users.noreply.github.com>
2019-01-29 15:03:12 +0300

    Create text_editor.py

---
+++ text_editor.py
@@ -0,0 +1,75 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QFileDialog
+from PyQt5.QtGui import QPixmap
+import sys
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.editor = QtWidgets.QTextEdit(self.centralwidget)
+        self.editor.setGeometry(QtCore.QRect(20, 40, 771, 521))
+        self.editor.setObjectName("editor")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(430, 10, 351, 25))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.create_new_btn = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.create_new_btn.setObjectName("create_new_btn")
+        self.horizontalLayout.addWidget(self.create_new_btn)
+        self.load_btn = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.load_btn.setObjectName("load_btn")
+        self.horizontalLayout.addWidget(self.load_btn)
+        self.save_btn = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.save_btn.setObjectName("save_btn")
+        self.horizontalLayout.addWidget(self.save_btn)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.create_new_btn.setText(_translate("MainWindow", "Создать новый"))
+        self.load_btn.setText(_translate("MainWindow", "Загрузить..."))
+        self.save_btn.setText(_translate("MainWindow", "Сохранить..."))
+
+
+class TextEditor(Ui_MainWindow, QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi()
+        self.load_btn.clicked.connect(self.load_form)
+        self.save_btn.clicked.connect(self.save_form)
+        self.create_new_btn.clicked.connect(self.create_form)
+
+    def load_form(self):
+        pass
+
+    def save_form(self):
+        pass
+
+    def create_form(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = TextEditor()
+    ex.show()
+    sys.exit(app.exec())
